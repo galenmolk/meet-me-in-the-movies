31c170517a60839169f5f094a31582c21429d312
gmolk <galenmolk@gmail.com>
2024-10-30 01:14:07 -0700

    fixing links

--- anthem/src/MenuButton.jsx
+++ anthem/src/MenuButton.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function MenuButton({button}) {
     const handleClick = () => {
         window.open(button.url);
@@ -5,12 +7,16 @@ function MenuButton({button}) {
 
     return (
         <div className='menu-link col-12 col-xl-4 mb-4'>
+        <Link to={button.url} target={null} style={{ textDecoration: 'none'}}>
+
             <div onClick={handleClick} className="menu-button">
                 <div className="menu-button-bg">
                     <img src='/mall-cop.png' alt="Mall Cop"></img>
                 </div>
                 <p>{button.title}</p>
             </div>
+        </Link>
+
         </div>
     );
 }
