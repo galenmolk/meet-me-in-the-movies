394f1f8a4cead0f147f43c95aa2101106c104538
Galen Molk <galenmolk@gmail.com>
2024-12-20 22:55:32 -0800

    allowing mobile entry

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import config from './Config';
 
+const CLICK_ENTER_COUNT = 16;
+let clickCount = 0;
+
 const createSite = () => {
   root.render(
     <StrictMode>
@@ -26,6 +29,14 @@ createSite();
 
 document.addEventListener('keydown', (event) => {
   if (event.ctrlKey && event.altKey && event.key === '8') {
+    config.isPrototype = false;
+    createSite();
+  }
+});
+
+window.addEventListener('click', (event) => {
+  clickCount++;
+  if (clickCount >= CLICK_ENTER_COUNT) {
     config.isPrototype = false;
     createSite();
   }
