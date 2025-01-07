f8fd038e9b2b6d4cef78ebca35884dc05179a7b7
Galen Molk <galenmolk@gmail.com>
2025-01-06 23:58:36 -0500

    style updates

--- src/MenuButton.jsx
+++ src/MenuButton.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 
+/*
+style={{ height: `${button.height}%`}}
+*/
+
 function MenuButton({button}) {
     return (
         <div className="menu-button fade-in" style={{ animationDelay: `${button.delay}s`}}>
             <Link to={button.url} target={"_blank"} style={{ textDecoration: 'none'}}>
                 {button.imgSrc !== undefined ? 
-                    (<img src={button.imgSrc} alt={button.alt} style={{ height: `${button.height}%`}}></img>) :
+                    (<img src={button.imgSrc} alt={button.alt} ></img>) :
                     (<></>)}
             </Link>
         </div>
