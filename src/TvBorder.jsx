aef3d5319e80750535bd3aaa7dbf49377777b4c6
Galen Molk <galenmolk@gmail.com>
2025-01-22 22:54:25 -0500

    removing tv border corners

--- src/TvBorder.jsx
+++ src/TvBorder.jsx
@@ -2,10 +2,6 @@ function TvBorder() {
     return (
         <> 
             <div className="border-overlay"></div>
-            <div className="border-corner top-left"></div>
-            <div className="border-corner top-right"></div>
-            <div className="border-corner bottom-left"></div>
-            <div className="border-corner bottom-right"></div>
             <div className="border-bar top-bar"></div>
             <div className="border-bar bottom-bar"></div>
         </>
