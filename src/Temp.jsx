3201e68a4f788674762d0174de66f9264aa275ea
Galen Molk <galenmolk@gmail.com>
2025-01-22 23:07:36 -0500

    cleaning up prototype page

--- src/Temp.jsx
+++ src/Temp.jsx
@@ -1,12 +1,8 @@
-import SocialCard from "./SocialCard";
-import TvBorder from "./TvBorder";
 import { Link } from "react-router-dom";
 
 export default function Temp() {
     return (
         <>
-        <TvBorder />
-
         <div className='teaser-container'>
             <Link to={'https://www.instagram.com/oliviawendelmusic'} target={"_blank"} style={{ textDecoration: 'none'}}>
                 <p className='teaser-text'>Coming Soon...</p>
