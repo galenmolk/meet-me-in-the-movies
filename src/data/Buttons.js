cc6f409262c9562ab5afb4e2b47d63b465b9e4bb
Galen Molk <galenmolk@gmail.com>
2025-03-13 22:07:42 -0400

    alt text improvements

--- src/data/Buttons.js
+++ src/data/Buttons.js
@@ -3,7 +3,7 @@ const buttons = [
     title: "presave", 
     url: "https://ffm.to/oliviawendel.OWE", 
     imgSrc: "./presave.png", 
-    alt: "Olivia Wendel Presave", 
+    alt: "Olivia Wendel Music Presave", 
     height: "115",
     delay: "0"
   },
@@ -11,7 +11,7 @@ const buttons = [
     title: "watch", 
     url: "https://www.youtube.com/watch?v=tfrulntFfOk",
     imgSrc: "./watch.png", 
-    alt: "Olivia Wendel Watch", 
+    alt: "Olivia Wendel Music Watch", 
     height: "115",
     delay: "0.1"
   },
@@ -19,7 +19,7 @@ const buttons = [
     title: "listen", 
     url: "https://open.spotify.com/artist/6VFhe94v6lnp9FLqRdfgAx?si=ums1yNPbRe2p5PdYbdzMtw", 
     imgSrc:"./listen.png",  
-    alt: "Olivia Wendel on Spotify", 
+    alt: "Olivia Wendel on Spotify Music", 
     height: "115",
     delay: "0.2"
   },
@@ -27,7 +27,7 @@ const buttons = [
     title: "follow", 
     url: "https://www.instagram.com/oliviawendelmusic/", 
     imgSrc: "./follow.png", 
-    alt: "Olivia Wendel Follow", 
+    alt: "Olivia Wendel Music Follow", 
     height: "115",
     delay: "0.3"
   },
@@ -35,7 +35,7 @@ const buttons = [
     title: "outtakes", 
     url: "https://youtu.be/OwbS4PGqBEo?si=xACFv-ulRTXU_lN2&t=268",
     imgSrc:"./outtakes.png", 
-    alt: "Olivia Wendel Outtakes", 
+    alt: "Olivia Wendel Music Outtakes Video", 
     height: "115",
     delay: "0.4"
   },
@@ -43,7 +43,7 @@ const buttons = [
     title: "merch", 
     url: "https://www.instagram.com/oliviawendelmusic", 
     imgSrc: "./merch.png", 
-    alt: "Olivia Wendel Merch", 
+    alt: "Olivia Wendel Music Merch", 
     height: "115",
     delay: "0.5"
   },
